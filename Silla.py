ab1a7a33e2a33afe74e9187216865edc3b9a3cb1
Felipe Alexander Ángel <63269119+GoldenEel@users.noreply.github.com>
2021-09-14 23:41:56 -0500

    Add files via upload

--- Silla.py
+++ Silla.py
@@ -1,5 +1,5 @@
 #Dict de alfabeto para traducir los rows a letras
-alfabeto_dict = {
+alfabeto= {
     1 : 'A', 2 : 'B', 3: 'C', 4 : 'D', 5 : 'E', 6 : 'F', 7 : 'G', 8 : 'H', 9 : 'I', 10 : 'J', 11: 'K', 12 : 'L', 13 : 'M', 14 : 'N' , 15 : 'O', 16 : 'P', 17 : 'Q' , 18 : 'R', 19 : 'S', 20 : 'T', 21 : 'U', 22 : 'V' ,
     23 : 'W', 24 : 'X', 25 : 'Y', 26 : 'Z'
 }
@@ -8,7 +8,7 @@ class Silla:
         #Como las sillas se crean cuando creamos la sala, su fila y columna le dan su nombre
         self.row = row
         self.column = str(col)
-        self.idSilla = alfabeto_dict[row] + self.column
+        self.idSilla = alfabeto[row] + self.column
         self.sillaOcupada = False
         #nombreSilla se usa para cambiar el nombre de silla en printSillas() cuando la silla esta ocupada sin tener que cambiar el identificador idSilla
         self.nombreSilla = self.idSilla
