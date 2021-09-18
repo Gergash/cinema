2a11c17becd8efdcfa27b24071f093df1f9b085f
Felipe Alexander Ángel <63269119+GoldenEel@users.noreply.github.com>
2021-09-18 18:23:28 -0500

    Add files via upload

---
+++ Genero.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class Genero(Enum):
+    Drama = 1
+    Accion = 2
+    Suspenso = 3
+    Romance = 4
+    Alternativa = 5
+    Comedia = 6
+    Historia = 7
+    Misterio = 8
+    Horror = 9
+    Musical = 10
+    Animacion = 11
+    Fantastica = 12
+    Documental = 13
+    UNSPECIFIED = 14
