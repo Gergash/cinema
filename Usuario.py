90469ee94bf36f7c18e839b9adf9386fe2ef07fa
Felipe Alexander Ángel <63269119+GoldenEel@users.noreply.github.com>
2021-09-18 18:26:58 -0500

    Add files via upload

---
+++ Usuario.py
@@ -0,0 +1,13 @@
+
+class Usuario:
+    def __init__(self, idUsuario, nomUsuario, fechaNacimiento, membresia):
+        self.idUsuario = idUsuario
+        self.nomUsuario = nomUsuario
+        self.fechaNachimiento = fechaNacimiento
+        self.membresia =
+    def getIdUsuario(self):
+        return self.idUsuario
+    def getNomUsuario(self):
+        return self.nomUsuario
+    def getFechaNacimiento(self):
+        return self.fechaNachimiento
